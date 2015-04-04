0dd139caa2e92403e1858366acdb97892525349b
h1994st <h1994st@gmail.com>
2015-04-04 17:14:21 +0800

    Initial Commit

---
+++ ME310FarmAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ME310FarmAssistant
+//
+//  Created by Tom Hu on 4/4/15.
+//  Copyright (c) 2015 Tom Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ME310FarmAssistant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ME310FarmAssistant
+//
+//  Created by Tom Hu on 4/4/15.
+//  Copyright (c) 2015 Tom Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
